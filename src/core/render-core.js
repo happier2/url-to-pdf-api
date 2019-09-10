d02c390c0e56c897e36db21d4b4570a0593fe71d
liuxiaoming1 <liuxiaoming1@cmbc.com.cn>
2019-09-10 15:58:51 +0800

    fix: change console event message to debug level

--- src/core/render-core.js
+++ src/core/render-core.js
@@ -46,7 +46,7 @@ async function render(_opts = {}) {
   });
   const page = await browser.newPage();
 
-  page.on('console', (...args) => logger.info('PAGE LOG:', ...args));
+  page.on('console', (...args) => logger.debug('PAGE LOG:', ...args));
 
   page.on('error', (err) => {
     logger.error(`Error event emitted: ${err}`);
